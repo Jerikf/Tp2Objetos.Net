a65a179444e4890bf9399bd94f3582f940dbf896
Erik <jerifkc@gmail.com>
2022-01-17 01:57:22 -0300

    Se creo el Formulario visual de agregar paciente

---
+++ AdministradorDePacientes/Vista/Forms/IngresarPaciente.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace Vista.Forms
+{
+    partial class IngresarPaciente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textId = new System.Windows.Forms.TextBox();
+            this.textLastName = new System.Windows.Forms.TextBox();
+            this.textAge = new System.Windows.Forms.TextBox();
+            this.textDni = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxObraSocial = new System.Windows.Forms.ComboBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textName
+            // 
+            this.textName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
+            this.textName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textName.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textName.Location = new System.Drawing.Point(163, 166);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(176, 16);
+            this.textName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label2.Location = new System.Drawing.Point(103, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 42);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Paciente";
+            // 
+            // textId
+            // 
+            this.textId.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
+            this.textId.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textId.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textId.Location = new System.Drawing.Point(163, 112);
+            this.textId.Name = "textId";
+            this.textId.Size = new System.Drawing.Size(176, 16);
+            this.textId.TabIndex = 4;
+            // 
+            // textLastName
+            // 
+            this.textLastName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
+            this.textLastName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textLastName.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textLastName.Location = new System.Drawing.Point(163, 216);
+            this.textLastName.Name = "textLastName";
+            this.textLastName.Size = new System.Drawing.Size(176, 16);
+            this.textLastName.TabIndex = 6;
+            // 
+            // textAge
+            // 
+            this.textAge.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
+            this.textAge.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textAge.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textAge.Location = new System.Drawing.Point(163, 270);
+            this.textAge.Name = "textAge";
+            this.textAge.Size = new System.Drawing.Size(176, 16);
+            this.textAge.TabIndex = 8;
+            // 
+            // textDni
+            // 
+            this.textDni.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
+            this.textDni.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textDni.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textDni.Location = new System.Drawing.Point(163, 329);
+            this.textDni.Name = "textDni";
+            this.textDni.Size = new System.Drawing.Size(176, 16);
+            this.textDni.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label6.Location = new System.Drawing.Point(89, 270);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 21);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Edad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label5.Location = new System.Drawing.Point(103, 329);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Dni";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label4.Location = new System.Drawing.Point(67, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Apellido";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label7.Location = new System.Drawing.Point(38, 392);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 21);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Obra Social";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(68, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label3.Location = new System.Drawing.Point(115, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Id";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label3.UseWaitCursor = true;
+            // 
+            // comboBoxObraSocial
+            // 
+            this.comboBoxObraSocial.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
+            this.comboBoxObraSocial.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBoxObraSocial.ForeColor = System.Drawing.Color.Gainsboro;
+            this.comboBoxObraSocial.FormattingEnabled = true;
+            this.comboBoxObraSocial.Items.AddRange(new object[] {
+            "No tiene",
+            "Cobertura Básica",
+            "Cobertura Completa"});
+            this.comboBoxObraSocial.Location = new System.Drawing.Point(163, 394);
+            this.comboBoxObraSocial.Name = "comboBoxObraSocial";
+            this.comboBoxObraSocial.Size = new System.Drawing.Size(176, 23);
+            this.comboBoxObraSocial.TabIndex = 12;
+            this.comboBoxObraSocial.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(54)))), ((int)(((byte)(112)))));
+            this.btnAceptar.FlatAppearance.BorderSize = 0;
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAceptar.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnAceptar.Location = new System.Drawing.Point(68, 462);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnAceptar.Size = new System.Drawing.Size(107, 29);
+            this.btnAceptar.TabIndex = 13;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(54)))), ((int)(((byte)(112)))));
+            this.btnCancelar.FlatAppearance.BorderSize = 0;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCancelar.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnCancelar.Location = new System.Drawing.Point(218, 462);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnCancelar.Size = new System.Drawing.Size(107, 29);
+            this.btnCancelar.TabIndex = 14;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // IngresarPaciente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(41)))), ((int)(((byte)(81)))));
+            this.ClientSize = new System.Drawing.Size(394, 532);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.comboBoxObraSocial);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textDni);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textAge);
+            this.Controls.Add(this.textLastName);
+            this.Controls.Add(this.textId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textName);
+            this.Name = "IngresarPaciente";
+            this.Text = "IngresarPaciente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textId;
+        private System.Windows.Forms.TextBox textLastName;
+        private System.Windows.Forms.TextBox textAge;
+        private System.Windows.Forms.TextBox textDni;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxObraSocial;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
