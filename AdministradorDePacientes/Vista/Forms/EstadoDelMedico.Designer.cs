9ed22f5533cf4a11d35a0c80367dff50c2fb8388
Erik <jerifkc@gmail.com>
2022-01-18 00:18:01 -0300

    Se creó el form visual del estado del médico

---
+++ AdministradorDePacientes/Vista/Forms/EstadoDelMedico.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace Vista.Forms
+{
+    partial class EstadoDelMedico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textDni = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label2.Location = new System.Drawing.Point(74, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 42);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Médico";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label3.Location = new System.Drawing.Point(17, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 21);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Nombre";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label3.UseWaitCursor = true;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label5.Location = new System.Drawing.Point(55, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 21);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Dni";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textDni
+            // 
+            this.textDni.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textDni.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
+            this.textDni.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textDni.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textDni.Location = new System.Drawing.Point(112, 161);
+            this.textDni.Name = "textDni";
+            this.textDni.Size = new System.Drawing.Size(176, 15);
+            this.textDni.TabIndex = 25;
+            // 
+            // textName
+            // 
+            this.textName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
+            this.textName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textName.ForeColor = System.Drawing.Color.Gainsboro;
+            this.textName.Location = new System.Drawing.Point(112, 109);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(176, 15);
+            this.textName.TabIndex = 24;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnConsultar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(54)))), ((int)(((byte)(112)))));
+            this.btnConsultar.FlatAppearance.BorderSize = 0;
+            this.btnConsultar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConsultar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnConsultar.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnConsultar.Location = new System.Drawing.Point(87, 230);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnConsultar.Size = new System.Drawing.Size(136, 51);
+            this.btnConsultar.TabIndex = 29;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            // 
+            // EstadoDelMedico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(41)))), ((int)(((byte)(81)))));
+            this.ClientSize = new System.Drawing.Size(317, 371);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textDni);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Gainsboro;
+            this.Name = "EstadoDelMedico";
+            this.Text = "Estado Del Médico";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textDni;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Button btnConsultar;
+    }
+}
