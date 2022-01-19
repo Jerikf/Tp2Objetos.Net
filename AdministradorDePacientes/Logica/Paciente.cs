652ec3b2244e6f6800a19ef79ef45666297ef66e
Erik <jerifkc@gmail.com>
2022-01-19 02:27:56 -0300

    Se implementó la clase Paciente

---
+++ AdministradorDePacientes/Logica/Paciente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logica
+{
+    public enum ObraSocial
+    {
+        NoTiene,
+        CoberturaBasica,
+        CoberturaCompleta
+    };
+
+    public class Paciente : Persona
+    {
+        private int numeroDeLlegada;
+        private ObraSocial tipo;
+
+        public Paciente(string nombre, string apellido, string dni, int edad, int numeroDeLlegada, ObraSocial tipo) : base(nombre, apellido, dni, edad)
+        {
+            this.numeroDeLlegada = numeroDeLlegada;
+            this.tipo = tipo;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Paciente paciente &&
+                   Dni == paciente.Dni;
+        }
+
+        public override void miInfo()
+        {
+            Console.WriteLine(base.toString() + $"Número de llegada : {this.numeroDeLlegada} \n Obra social : {tipo}");
+        }
+
+        public override string Saludar()
+        {
+            return $"{base.Saludar()} Paciente";
+        }
+    }   
+}
