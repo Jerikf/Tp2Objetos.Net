45c8f03c7f97210a47d8b9c97b3937b45d5f39a7
Erik <jerifkc@gmail.com>
2022-01-21 02:26:22 -0300

    Logica- Se creó la carpeta viewModel donde se implementó la clase GetMedico, se usa para mostrar la información requerida

---
+++ AdministradorDePacientes/Logica/ViewModel/GetMedicoVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logica.ViewModel
+{
+    public class GetMedicoVM
+    {
+        private string nombre;
+        private string apellido;
+        private string especialidad;
+
+        public GetMedicoVM(string nombre, string apellido, string especialidad)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.especialidad = especialidad;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Especialidad { get => especialidad; set => especialidad = value; }
+    }
+}
