5dcc01fd852d09e7ab76ae201c7c5046864276f8
Erik <jerifkc@gmail.com>
2022-01-24 01:54:11 -0300

    Creación de las validaciones de Entrada como, nombre, apellido, dni, edad

---
+++ AdministradorDePacientes/Common/validations/StringValidationsExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Common.validations
+{
+    public static class StringValidationsExtensions
+    {
+        public static bool DniValidation(this string dni)
+        {
+            Regex regex = new Regex(@"^([0-9]{8}|[0-9]{2}\-[0-9]{3}\-[0-9]{3}|[0-9]{2}\.[0-9]{3}\.[0-9]{3})$");
+            return regex.IsMatch(dni);
+        }
+        public static bool StringValidation(this string cadena)
+        {
+            Regex regex = new Regex(@"^([a-zA-Z]+)\s?([a-zA-Z]+)$");
+            return regex.IsMatch(cadena);
+        }
+         
+        public static bool AgeValidation(this string age)
+        {
+            Regex regex = new Regex(@"^\d+$");
+            return (regex.IsMatch(age) && Int32.Parse(age) > 0 && Int32.Parse(age) < 112);
+        }
+    }
+}
