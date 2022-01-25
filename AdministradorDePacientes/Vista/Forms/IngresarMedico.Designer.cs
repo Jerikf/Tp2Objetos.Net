54c462b42574e92f3959e55f952f04559de02ceb
Erik <jerifkc@gmail.com>
2022-01-25 20:06:08 -0300

    Se quitó el campo(label + comboBox -> Si está atendiendo) del form visual

--- AdministradorDePacientes/Vista/Forms/IngresarMedico.Designer.cs
+++ AdministradorDePacientes/Vista/Forms/IngresarMedico.Designer.cs
@@ -29,12 +29,11 @@ namespace Vista.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.btnAceptar = new System.Windows.Forms.Button();
-            this.comboBoxDisponible = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -44,6 +43,18 @@ namespace Vista.Forms
             this.label2 = new System.Windows.Forms.Label();
             this.textLastName = new System.Windows.Forms.TextBox();
             this.comboBoxEspecialidad = new System.Windows.Forms.ComboBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider4 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider5 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider6 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider6)).BeginInit();
             this.SuspendLayout();
             // 
             // btnCancelar
@@ -54,7 +65,7 @@ namespace Vista.Forms
             this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancelar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnCancelar.ForeColor = System.Drawing.Color.Gainsboro;
-            this.btnCancelar.Location = new System.Drawing.Point(179, 323);
+            this.btnCancelar.Location = new System.Drawing.Point(188, 323);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.btnCancelar.Size = new System.Drawing.Size(107, 29);
@@ -70,7 +81,7 @@ namespace Vista.Forms
             this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAceptar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnAceptar.ForeColor = System.Drawing.Color.Gainsboro;
-            this.btnAceptar.Location = new System.Drawing.Point(32, 323);
+            this.btnAceptar.Location = new System.Drawing.Point(41, 323);
             this.btnAceptar.Name = "btnAceptar";
             this.btnAceptar.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.btnAceptar.Size = new System.Drawing.Size(107, 29);
@@ -78,28 +89,13 @@ namespace Vista.Forms
             this.btnAceptar.Text = "Aceptar";
             this.btnAceptar.UseVisualStyleBackColor = false;
             // 
-            // comboBoxDisponible
-            // 
-            this.comboBoxDisponible.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.comboBoxDisponible.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
-            this.comboBoxDisponible.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.comboBoxDisponible.ForeColor = System.Drawing.Color.Gainsboro;
-            this.comboBoxDisponible.FormattingEnabled = true;
-            this.comboBoxDisponible.Items.AddRange(new object[] {
-            "SI",
-            "NO"});
-            this.comboBoxDisponible.Location = new System.Drawing.Point(131, 263);
-            this.comboBoxDisponible.Name = "comboBoxDisponible";
-            this.comboBoxDisponible.Size = new System.Drawing.Size(176, 23);
-            this.comboBoxDisponible.TabIndex = 27;
-            // 
             // label1
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label1.Location = new System.Drawing.Point(36, 112);
+            this.label1.Location = new System.Drawing.Point(45, 133);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(74, 21);
             this.label1.TabIndex = 15;
@@ -111,7 +107,7 @@ namespace Vista.Forms
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label3.Location = new System.Drawing.Point(36, 76);
+            this.label3.Location = new System.Drawing.Point(45, 90);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(73, 21);
             this.label3.TabIndex = 18;
@@ -119,25 +115,13 @@ namespace Vista.Forms
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
             this.label3.UseWaitCursor = true;
             // 
-            // label7
-            // 
-            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label7.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label7.Location = new System.Drawing.Point(21, 261);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(89, 21);
-            this.label7.TabIndex = 26;
-            this.label7.Text = "Disponible";
-            // 
             // label6
             // 
             this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label6.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label6.Location = new System.Drawing.Point(58, 189);
+            this.label6.Location = new System.Drawing.Point(67, 217);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(52, 21);
             this.label6.TabIndex = 24;
@@ -149,7 +133,7 @@ namespace Vista.Forms
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label5.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label5.Location = new System.Drawing.Point(74, 150);
+            this.label5.Location = new System.Drawing.Point(81, 177);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(35, 21);
             this.label5.TabIndex = 22;
@@ -163,7 +147,7 @@ namespace Vista.Forms
             this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label4.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label4.Location = new System.Drawing.Point(1, 223);
+            this.label4.Location = new System.Drawing.Point(8, 268);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(109, 21);
             this.label4.TabIndex = 20;
@@ -175,7 +159,7 @@ namespace Vista.Forms
             this.textAge.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
             this.textAge.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textAge.ForeColor = System.Drawing.Color.Gainsboro;
-            this.textAge.Location = new System.Drawing.Point(131, 194);
+            this.textAge.Location = new System.Drawing.Point(140, 222);
             this.textAge.Name = "textAge";
             this.textAge.Size = new System.Drawing.Size(176, 16);
             this.textAge.TabIndex = 23;
@@ -186,7 +170,7 @@ namespace Vista.Forms
             this.textDni.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
             this.textDni.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textDni.ForeColor = System.Drawing.Color.Gainsboro;
-            this.textDni.Location = new System.Drawing.Point(131, 155);
+            this.textDni.Location = new System.Drawing.Point(138, 182);
             this.textDni.Name = "textDni";
             this.textDni.Size = new System.Drawing.Size(176, 16);
             this.textDni.TabIndex = 21;
@@ -197,7 +181,7 @@ namespace Vista.Forms
             this.textName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
             this.textName.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textName.ForeColor = System.Drawing.Color.Gainsboro;
-            this.textName.Location = new System.Drawing.Point(131, 81);
+            this.textName.Location = new System.Drawing.Point(140, 95);
             this.textName.Name = "textName";
             this.textName.Size = new System.Drawing.Size(176, 16);
             this.textName.TabIndex = 19;
@@ -208,7 +192,7 @@ namespace Vista.Forms
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label2.Location = new System.Drawing.Point(83, 18);
+            this.label2.Location = new System.Drawing.Point(99, 18);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(149, 42);
             this.label2.TabIndex = 17;
@@ -220,7 +204,7 @@ namespace Vista.Forms
             this.textLastName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
             this.textLastName.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textLastName.ForeColor = System.Drawing.Color.Gainsboro;
-            this.textLastName.Location = new System.Drawing.Point(131, 117);
+            this.textLastName.Location = new System.Drawing.Point(140, 138);
             this.textLastName.Name = "textLastName";
             this.textLastName.Size = new System.Drawing.Size(176, 16);
             this.textLastName.TabIndex = 16;
@@ -239,24 +223,46 @@ namespace Vista.Forms
             "Hurología",
             "Podología",
             "Oftamología"});
-            this.comboBoxEspecialidad.Location = new System.Drawing.Point(131, 223);
+            this.comboBoxEspecialidad.Location = new System.Drawing.Point(138, 268);
             this.comboBoxEspecialidad.Name = "comboBoxEspecialidad";
             this.comboBoxEspecialidad.Size = new System.Drawing.Size(176, 23);
             this.comboBoxEspecialidad.TabIndex = 30;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            this.errorProvider3.ContainerControl = this;
+            // 
+            // errorProvider4
+            // 
+            this.errorProvider4.ContainerControl = this;
+            // 
+            // errorProvider5
+            // 
+            this.errorProvider5.ContainerControl = this;
+            // 
+            // errorProvider6
+            // 
+            this.errorProvider6.ContainerControl = this;
             // 
             // IngresarMedico
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(41)))), ((int)(((byte)(81)))));
-            this.ClientSize = new System.Drawing.Size(317, 371);
+            this.ClientSize = new System.Drawing.Size(335, 371);
             this.Controls.Add(this.comboBoxEspecialidad);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnAceptar);
-            this.Controls.Add(this.comboBoxDisponible);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -267,6 +273,13 @@ namespace Vista.Forms
             this.Controls.Add(this.textLastName);
             this.Name = "IngresarMedico";
             this.Text = "Ingresar Médico";
+            this.Load += new System.EventHandler(this.IngresarMedico_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -276,10 +289,8 @@ namespace Vista.Forms
 
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Button btnAceptar;
-        private System.Windows.Forms.ComboBox comboBoxDisponible;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
@@ -289,5 +300,11 @@ namespace Vista.Forms
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textLastName;
         private System.Windows.Forms.ComboBox comboBoxEspecialidad;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+        private System.Windows.Forms.ErrorProvider errorProvider4;
+        private System.Windows.Forms.ErrorProvider errorProvider5;
+        private System.Windows.Forms.ErrorProvider errorProvider6;
     }
 }
