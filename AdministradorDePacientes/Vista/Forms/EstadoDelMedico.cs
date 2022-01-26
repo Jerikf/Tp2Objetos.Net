ed0f88784c6306538666fa3009accb0f58f1160f
Erik <jerifkc@gmail.com>
2022-01-25 21:32:57 -0300

    Se implementó la Acción de poder consultar el estado(si está disponible o ocupado) de un médico

--- AdministradorDePacientes/Vista/Forms/EstadoDelMedico.cs
+++ AdministradorDePacientes/Vista/Forms/EstadoDelMedico.cs
@@ -1,4 +1,5 @@
-﻿using Logica;
+﻿using Common.validations;
+using Logica;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,6 +19,54 @@ namespace Vista.Forms
         {
             InitializeComponent();
             this.clinica = clinica;
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            if(textName.Text.StringValidation() & textDni.Text.DniValidation())
+            {
+ 
+                List<Medico> medicos = clinica.Medicos;
+                int pos = 0;
+                bool seEncontro = false;
+                while(pos < medicos.Count & !seEncontro)
+                {
+                    if(medicos[pos].Dni == textDni.Text & medicos[pos].Nombre == textName.Text)
+                        seEncontro = true;
+                    pos++;
+                }
+
+                if(seEncontro)
+                    MessageBox.Show($"Médico {(medicos[--pos].Atendiendo ? "OCUPADO" : "DISPONIBLE")}", "Clinica", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show("El MÉDICO NO EXISTE EN NUESTRA  CLÍNICA", "Clinica" , MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textName.Text = "";
+                textDni.Text = "";
+
+            }
+            else
+            {
+                MessageBox.Show("Ingrese todos los datos correctos por favor", "Clinica", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void textName_Validating(object sender, CancelEventArgs e)
+        {
+            if (!textName.Text.StringValidation())
+                errorProvider1.SetError(textName, "Ingrese un Nombre correcto");
+            else
+                errorProvider1.Clear();
+
+        }
+
+        private void textDni_Validating(object sender, CancelEventArgs e)
+        {
+            if (!textDni.Text.DniValidation())
+                errorProvider2.SetError(textDni, "Ingrese un DNI correcto");
+            else
+                errorProvider2.Clear();
+
         }
     }
 }
