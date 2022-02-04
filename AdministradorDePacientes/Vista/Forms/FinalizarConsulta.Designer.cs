cae98d106ac2d6c47681221ad87704bd34be5d39
Erik <jerifkc@gmail.com>
2022-02-04 00:16:17 -0300

    Se creo el Formulario para Finalizar una Consulta Médica

---
+++ AdministradorDePacientes/Vista/Forms/FinalizarConsulta.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Vista.Forms
+{
+    partial class FinalizarConsulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBoxMedicos = new System.Windows.Forms.ComboBox();
+            this.comboBoxPaciente = new System.Windows.Forms.ComboBox();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxMedicos
+            // 
+            this.comboBoxMedicos.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBoxMedicos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
+            this.comboBoxMedicos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBoxMedicos.ForeColor = System.Drawing.Color.Gainsboro;
+            this.comboBoxMedicos.FormattingEnabled = true;
+            this.comboBoxMedicos.Location = new System.Drawing.Point(108, 98);
+            this.comboBoxMedicos.Name = "comboBoxMedicos";
+            this.comboBoxMedicos.Size = new System.Drawing.Size(176, 25);
+            this.comboBoxMedicos.TabIndex = 32;
+            this.comboBoxMedicos.Text = "Seleccionar";
+            this.comboBoxMedicos.SelectedIndexChanged += new System.EventHandler(this.comboBoxMedicos_SelectedIndexChanged);
+            this.comboBoxMedicos.Validating += new System.ComponentModel.CancelEventHandler(this.comboBoxMedicos_Validating);
+            // 
+            // comboBoxPaciente
+            // 
+            this.comboBoxPaciente.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBoxPaciente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
+            this.comboBoxPaciente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBoxPaciente.ForeColor = System.Drawing.Color.Gainsboro;
+            this.comboBoxPaciente.FormattingEnabled = true;
+            this.comboBoxPaciente.Location = new System.Drawing.Point(108, 182);
+            this.comboBoxPaciente.Name = "comboBoxPaciente";
+            this.comboBoxPaciente.Size = new System.Drawing.Size(176, 25);
+            this.comboBoxPaciente.TabIndex = 33;
+            this.comboBoxPaciente.Text = "Seleccionar";
+            this.comboBoxPaciente.Validating += new System.ComponentModel.CancelEventHandler(this.comboBoxPacientes_Validating);
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnFinalizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(54)))), ((int)(((byte)(112)))));
+            this.btnFinalizar.FlatAppearance.BorderSize = 0;
+            this.btnFinalizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFinalizar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnFinalizar.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnFinalizar.Location = new System.Drawing.Point(78, 279);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnFinalizar.Size = new System.Drawing.Size(146, 43);
+            this.btnFinalizar.TabIndex = 36;
+            this.btnFinalizar.Text = "Finalizar";
+            this.btnFinalizar.UseVisualStyleBackColor = false;
+            this.btnFinalizar.Click += new System.EventHandler(this.btnFinalizar_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label2.Location = new System.Drawing.Point(62, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 33);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Finalización ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label2.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label3.Location = new System.Drawing.Point(23, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 21);
+            this.label3.TabIndex = 38;
+            this.label3.Text = "Médico";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label3.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Gainsboro;
+            this.label1.Location = new System.Drawing.Point(23, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 21);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Paciente";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label1.UseWaitCursor = true;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // FinalizarConsulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(41)))), ((int)(((byte)(81)))));
+            this.ClientSize = new System.Drawing.Size(317, 371);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnFinalizar);
+            this.Controls.Add(this.comboBoxPaciente);
+            this.Controls.Add(this.comboBoxMedicos);
+            this.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Gainsboro;
+            this.Name = "FinalizarConsulta";
+            this.Text = "Finalizar Consulta";
+            this.Load += new System.EventHandler(this.FinalizarConsulta_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxMedicos;
+        private System.Windows.Forms.ComboBox comboBoxPaciente;
+        private System.Windows.Forms.Button btnFinalizar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+    }
+}
