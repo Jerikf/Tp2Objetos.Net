8ced0b0e4a5ff23c6dcdd8f6ce8d70a69df4d94f
Erik <jerifkc@gmail.com>
2022-02-05 02:34:29 -0300

    Se agrevo un VM para mostrar los médicos con sus cantidades de pacientes atendidos

---
+++ AdministradorDePacientes/Logica/ViewModel/GetMedicoStatisticsVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logica.ViewModel
+{
+    public class GetMedicoStatisticsVM
+    {
+        private string nombre;
+        private string especialidad;
+        private int cantDePacientesAtendidos;
+
+        public GetMedicoStatisticsVM(string nombre, string especialidad, int cantDePacientesAtendidos)
+        {
+            this.nombre = nombre;
+            this.especialidad = especialidad;
+            this.cantDePacientesAtendidos = cantDePacientesAtendidos;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Especialidad { get => especialidad; set => especialidad = value; }
+        public int CantDePacientesAtendidos { get => cantDePacientesAtendidos; set => cantDePacientesAtendidos = value; }
+    }
+}
