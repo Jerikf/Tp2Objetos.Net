f80902f0463795612ecbe9223c35da0115358a8d
Erik <jerifkc@gmail.com>
2022-02-06 01:24:34 -0300

    Se Creo el Form(aspecto visual) de estadísticas

---
+++ AdministradorDePacientes/Vista/Forms/Estadisticas.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace Vista.Forms
+{
+    partial class Estadisticas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvPrueba2 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MedicoMenor = new System.Windows.Forms.Label();
+            this.MedicoMayor = new System.Windows.Forms.Label();
+            this.EspecialidadMax = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrueba2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPrueba2
+            // 
+            this.dgvPrueba2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dgvPrueba2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(30)))), ((int)(((byte)(62)))));
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(30)))), ((int)(((byte)(62)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvPrueba2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvPrueba2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPrueba2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(30)))), ((int)(((byte)(62)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Gainsboro;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvPrueba2.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvPrueba2.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(30)))), ((int)(((byte)(62)))));
+            this.dgvPrueba2.Location = new System.Drawing.Point(16, 22);
+            this.dgvPrueba2.Name = "dgvPrueba2";
+            this.dgvPrueba2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dgvPrueba2.RowTemplate.Height = 25;
+            this.dgvPrueba2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPrueba2.Size = new System.Drawing.Size(660, 131);
+            this.dgvPrueba2.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nombre";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Especialidad";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Cantidad de Pacientes Atendidos";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 250;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(16, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(308, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "El Médico que más atendió : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(16, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(333, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "El Médico que menos atendió : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(330, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 18);
+            this.label3.TabIndex = 6;
+            // 
+            // MedicoMenor
+            // 
+            this.MedicoMenor.AutoSize = true;
+            this.MedicoMenor.Font = new System.Drawing.Font("Century Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.MedicoMenor.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.MedicoMenor.Location = new System.Drawing.Point(346, 253);
+            this.MedicoMenor.Name = "MedicoMenor";
+            this.MedicoMenor.Size = new System.Drawing.Size(18, 25);
+            this.MedicoMenor.TabIndex = 7;
+            this.MedicoMenor.Text = " ";
+            // 
+            // MedicoMayor
+            // 
+            this.MedicoMayor.AutoSize = true;
+            this.MedicoMayor.Font = new System.Drawing.Font("Century Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.MedicoMayor.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.MedicoMayor.Location = new System.Drawing.Point(330, 203);
+            this.MedicoMayor.Name = "MedicoMayor";
+            this.MedicoMayor.Size = new System.Drawing.Size(18, 25);
+            this.MedicoMayor.TabIndex = 8;
+            this.MedicoMayor.Text = " ";
+            // 
+            // EspecialidadMax
+            // 
+            this.EspecialidadMax.AutoSize = true;
+            this.EspecialidadMax.Font = new System.Drawing.Font("Century Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.EspecialidadMax.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.EspecialidadMax.Location = new System.Drawing.Point(390, 301);
+            this.EspecialidadMax.Name = "EspecialidadMax";
+            this.EspecialidadMax.Size = new System.Drawing.Size(18, 25);
+            this.EspecialidadMax.TabIndex = 10;
+            this.EspecialidadMax.Text = " ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Location = new System.Drawing.Point(16, 301);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(363, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "La Especialidad que más atendió :";
+            // 
+            // Estadisticas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(56)))), ((int)(((byte)(100)))));
+            this.ClientSize = new System.Drawing.Size(688, 377);
+            this.Controls.Add(this.EspecialidadMax);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.MedicoMayor);
+            this.Controls.Add(this.MedicoMenor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvPrueba2);
+            this.Name = "Estadisticas";
+            this.Text = "Estadisticas";
+            this.Load += new System.EventHandler(this.Estadisticas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrueba2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dgvPrueba2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label MedicoMenor;
+        private System.Windows.Forms.Label MedicoMayor;
+        private System.Windows.Forms.Label EspecialidadMax;
+        private System.Windows.Forms.Label label5;
+    }
+}
