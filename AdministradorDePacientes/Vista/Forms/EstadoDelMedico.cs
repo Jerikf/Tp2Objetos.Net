70e86f960ed1cc0edcb627e107f90a1710fcb0b6
Erik <jerifkc@gmail.com>
2022-02-09 01:05:24 -0300

    Se eliminó la implementación anterior, y se implementó el evento Load para la carga de los datos de los estados de los médicos

--- AdministradorDePacientes/Vista/Forms/EstadoDelMedico.cs
+++ AdministradorDePacientes/Vista/Forms/EstadoDelMedico.cs
@@ -21,52 +21,16 @@ namespace Vista.Forms
             this.clinica = clinica;
         }
 
-        private void btnConsultar_Click(object sender, EventArgs e)
+        private void EstadoDelMedico_Load(object sender, EventArgs e)
         {
-            if(textName.Text.StringValidation() & textDni.Text.DniValidation())
+            foreach (var medico in clinica.ObtenerMedicosStateVM())
             {
- 
-                List<Medico> medicos = clinica.Medicos;
-                int pos = 0;
-                bool seEncontro = false;
-                while(pos < medicos.Count & !seEncontro)
-                {
-                    if(medicos[pos].Dni == textDni.Text & medicos[pos].Nombre == textName.Text)
-                        seEncontro = true;
-                    pos++;
-                }
-
-                if(seEncontro)
-                    MessageBox.Show($"Médico {(medicos[--pos].Atendiendo ? "OCUPADO" : "DISPONIBLE")}", "Clinica", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                else
-                    MessageBox.Show("El MÉDICO NO EXISTE EN NUESTRA  CLÍNICA", "Clinica" , MessageBoxButtons.OK, MessageBoxIcon.Error);
-                textName.Text = "";
-                textDni.Text = "";
+                int index = dgvAtendiendo.Rows.Add();
+                dgvAtendiendo.Rows[index].Cells[0].Value = medico.Nombre;
+                dgvAtendiendo.Rows[index].Cells[1].Value = medico.Especialidad;
+                dgvAtendiendo.Rows[index].Cells[2].Value = medico.Atendiendo;
 
             }
-            else
-            {
-                MessageBox.Show("Ingrese todos los datos correctos por favor", "Clinica", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-
-        }
-
-        private void textName_Validating(object sender, CancelEventArgs e)
-        {
-            if (!textName.Text.StringValidation())
-                errorProvider1.SetError(textName, "Ingrese un Nombre correcto");
-            else
-                errorProvider1.Clear();
-
-        }
-
-        private void textDni_Validating(object sender, CancelEventArgs e)
-        {
-            if (!textDni.Text.DniValidation())
-                errorProvider2.SetError(textDni, "Ingrese un DNI correcto");
-            else
-                errorProvider2.Clear();
-
         }
     }
 }
