fdef6131188d2001d80e7e070402fb247e2db433
Erik <jerifkc@gmail.com>
2022-02-09 01:02:06 -0300

    Se agregó el VM del médico del Form estado del médico

---
+++ AdministradorDePacientes/Logica/ViewModel/GetMedicoStateVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logica.ViewModel
+{
+    public class GetMedicoStateVM
+    {
+        private string nombre;
+        private string especialidad;
+        private string atendiendo;
+
+        public GetMedicoStateVM(string nombre, string especialidad, string atendiendo)
+        {
+            this.nombre = nombre;
+            this.especialidad = especialidad;
+            this.atendiendo = atendiendo;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Especialidad { get => especialidad; set => especialidad = value; }
+        public string Atendiendo { get => atendiendo; set => atendiendo = value; }
+    }
+}
